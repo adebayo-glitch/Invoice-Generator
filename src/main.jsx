9c083ef390325ce9e1eac79cbe9590f1389f5105
adebayo-glitch <pillarbasetech@gmail.com>
2024-09-30 18:14:50 -0400

    update the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import invoiceReducer from './features/invoices/invoiceSlice';
 import App from './App.jsx'
-import './index.css'
+import GlobalStyles from './GlobalStyles';
+
+
+const store = configureStore({
+  reducer: {
+    invoice: invoiceReducer,
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <GlobalStyles />
+     <App />
+   </Provider>
   </StrictMode>,
 )
